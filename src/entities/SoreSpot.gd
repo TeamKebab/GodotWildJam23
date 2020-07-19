be5ef1df9c5efed03691a4d613e75bb7ef9522cc
Ángela López <angela.lbsr@gmail.com>
2020-07-19 11:03:48 +0200

    sore spot sounds

--- src/entities/SoreSpot.gd
+++ src/entities/SoreSpot.gd
@@ -9,10 +9,15 @@ onready var hurtbox : Area2D = $HurtBox
 onready var sprite : Sprite = $Sprite
 onready var timer : Timer = $Timer
 
+onready var appear_sound : AudioStreamPlayer2D = $AppearSound
+onready var infect_sound : AudioStreamPlayer2D = $InfectSound
+onready var heal_sound : AudioStreamPlayer2D = $HealSound
+
 
 func _ready() -> void:
 	timer.connect("timeout", self, "_on_timer_timeout")
 	timer.start(duration)
+	appear_sound.play()
 	
 
 func set_hp(new_hp) -> void:
@@ -25,7 +30,13 @@ func set_hp(new_hp) -> void:
 func _on_timer_timeout() -> void:
 	Player.heal()
 	#sprite.play("heal")
+	heal_sound.play()
+	
 	#yield(sprite, "animation_finished")	
+	hide()
+	
+	yield(heal_sound, "finished")
+	
 	queue_free()
 
 
@@ -33,6 +44,11 @@ func _die():
 	Player.infect()
 		
 	#sprite.play("infected")
+	infect_sound.play()
+	
 	#yield(sprite, "animation_finished")		
+	hide()
+	
+	yield(infect_sound, "finished")
 	queue_free()	
 	
